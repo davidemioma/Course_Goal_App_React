b7d1f4e89759f1502cf00daecad9454e05d1fe61
davidemioma <davidemiomauche@gmail.com>
2021-12-17 13:28:02 +0100

    page

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,10 +28,9 @@ function CourseInput(props) {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        <label>Course Goal</label>
         <input
-          style={{ borderColor: !isValid ? "red" : "#ccc" }}
           value={enteredValue}
           type="text"
           onChange={goalInputChangeHandler}
